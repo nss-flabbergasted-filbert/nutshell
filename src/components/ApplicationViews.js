bfcd5be10b11292d2322fa995af0b24a73bd7004
Abbey Brown <abbeybrownsemail@gmail.com>
2019-03-06 11:27:16 -0600

    nav links and routes

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,11 +1,31 @@
 import React, { Component } from "react"
+import { Route } from 'react-router-dom'
+
+import ChatList from './chat/ChatList'
+import NewsList from './news/NewsList'
+import EventList from './event/EventList'
+import TaskList from './task/TaskList'
 
 class ApplicationViews extends Component {
   state = {}
-  componentDidMount() {}
+  componentDidMount() { }
   render() {
     console.log(this.props.activeUser)
     return <React.Fragment>
+      <Route exact path="/chat" render={(props) => {
+        return <ChatList chat={this.state.chat} />
+      }} />
+       <Route exact path="/articles" render={(props) => {
+        return <NewsList news={this.state.news} />
+      }} />
+       <Route exact path="/events" render={(props) => {
+        return <EventList events={this.state.events} />
+      }} />
+       <Route exact path="/tasks" render={(props) => {
+        return <TaskList tasks={this.state.tasks} />
+      }} />
+      
+
     </React.Fragment>
   }
 }
