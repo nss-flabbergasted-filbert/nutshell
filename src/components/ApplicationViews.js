2778a93d5fdf52e53181382fa531e980dc9fa585
Niall Fraser <niallfraser92@gmail.com>
2019-03-06 11:29:36 -0600

    quick save before pull

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react"
 import IsAuth from "./Auth/IsAuth"
 class ApplicationViews extends Component {
-  state = {}
+  state = {
+    articles: []
+  }
   componentDidMount() {}
   render() {
     console.log(this.props.activeUser)
