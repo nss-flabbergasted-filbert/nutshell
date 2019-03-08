de1799a95f8728f83792ec5101009ccaac767211
Abbey Brown <abbeybrownsemail@gmail.com>
2019-03-08 13:26:12 -0600

    filtered by loggin in user

--- src/components/task/TaskList.js
+++ src/components/task/TaskList.js
@@ -6,6 +6,8 @@ import Task from './Task'
 
 export default class TaskList extends Component {
     render() {
+
+        const userId = parseInt(sessionStorage.getItem("credentials"))
         return (
             <React.Fragment>
                 <div className="taskButton">
@@ -20,7 +22,8 @@ export default class TaskList extends Component {
                 <section className="tasks">
 
                     {
-                        this.props.tasks.map(task =>
+                        this.props.tasks.filter(task => task.userId === userId)
+                        .map(task =>
                             <div className="task" key={task.id}>
                                 <Task task={task} {...this.props}
                                     {...this.props} users={this.props.users} 
